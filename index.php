890ccbf490f586aef0dde4c20d0ca2d031004d2a
Walter Varela <sen0rxol0@gmail.com>
2017-10-16 12:21:11 +0200

    Added basic structure for front office page

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+require_once('inc/init.inc.php');
+
+if ($_GET) { 
+// s'il y a une information dans l'url, c'est que l'internaute a clique sur l'un des liens
+    $file = $_GET['page'] . '.php';
+
+    if (file_exists($file)) {
+        require_once($file);
+    } else {
+        $content .= '<div class="alert alert-danger">La demande n\'a pas pu aboutir</div>';
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Boutik</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= URL ?>assets/css/styles.css">
+</head>
+<body>
+    <nav class="navbar navbar-dafault navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button class="navbar-toggle collapsed" type="button" 
+                    data-toggle="collapse" data-target="#navbar" 
+                    aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <a href="<?= URL ?>" class="navbar-brand">Boutique</a>
+            </div>
+
+            <div id="navbar" class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="?page=catalogue">Catalogue</a></li>
+                    <li><a href="?page=panier">Panier</a></li>
+                    <li><a href="?page=inscription">S&apos;inscrire</a></li>
+                    <li><a href="?page=connexion">Connexion</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="starter-template">
+            <h1>Front Office</h1>
+            <?= $content ?>
+        </div>
+    </div>
+</body>
+</html>
