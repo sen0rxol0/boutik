357ef352a4509c85c9d49965dc0feda92d15a8f9
Walter Varela <sen0rxol0@gmail.com>
2017-10-18 13:37:18 +0200

    Restructed folders

--- public/index.php
+++ public/index.php
@@ -1,10 +1,10 @@
 <?php
-require_once('inc/init.inc.php');
+require_once('../inc/init.inc.php');
 $titrepage = 'Boutik';
 
 if ($_GET) { 
 // s'il y a une information dans l'url, c'est que l'internaute a clique sur l'un des liens
-    $file = $_GET['page'] . '.php';
+    $file = RACINE . 'pages/' . $_GET['page'] . '.php';
 
     if (file_exists($file)) {
         require_once($file);
